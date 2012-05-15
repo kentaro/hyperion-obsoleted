f21283d6c34be08be35b1ef09dc46b497af69914
Kentaro Kuribayashi <kentarok@gmail.com>
2012-05-15 17:50:56 +0900

    rails new hyperion -T

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Hyperion::Application.config.secret_token = '9f0ba71609adf5b062cec95755f453acbb36bf7e4375357657882d8622a238608b0525a1ca6a2b420814e75811fb11c30860fd5f03c2952f5e354c048a01cde0'
