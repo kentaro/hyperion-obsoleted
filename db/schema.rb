47182b4e106a216310057b994ca9f66e6941bc78
Kentaro Kuribayashi <kentarok@gmail.com>
2012-05-16 13:43:25 +0900

    Re-init db migration.

--- db/schema.rb
+++ db/schema.rb
@@ -13,13 +13,6 @@
 
 ActiveRecord::Schema.define(:version => 20120516020739) do
 
-  create_table "hosts", :force => true do |t|
-    t.string   "name"
-    t.text     "description"
-    t.datetime "created_at",  :null => false
-    t.datetime "updated_at",  :null => false
-  end
-
   create_table "services", :force => true do |t|
     t.string   "name"
     t.text     "description"
